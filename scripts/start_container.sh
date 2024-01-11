955305d0ecb9c2f87b0645049b928a6ec856af5f
nandini-murthy <153975114+nandini-murthy@users.noreply.github.com>
2024-01-11 13:36:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull nandinimurthy/simple-python-flask-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 nandinimurthy/simple-python-flask-app
 
